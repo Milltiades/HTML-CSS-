8acb3abba002a121086c14f9b7da4b7723b13d3c
Milltiades <gigakakulia2017@gmail.com>
2023-11-17 23:02:19 +0400

    add hero bg and fixed navigation button

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
 navToggle.addEventListener("click", () => {
+  primaryNav.toggleAttribute("data-visible");
   primaryNav.hasAttribute("data-visible")
     ? navToggle.setAttribute("aria-expanded", true)
     : navToggle.setAttribute("aria-expanded", false);
-  primaryNav.toggleAttribute("data-visible");
+
   primaryHeader.toggleAttribute("data-overlay");
 });
 
